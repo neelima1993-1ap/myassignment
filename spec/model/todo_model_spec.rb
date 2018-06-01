6f25d44c4f75431cffee631e7a2d0086f1c73b0d
neelima1993-1ap <neelujoshi20@gmail.com>
2018-06-01 12:56:29 +0530

    Adding rspec

---
+++ spec/model/todo_model_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe Todo, :type => :model do
+  context "create a Todo" do
+    it "for the first project and first user" do
+      project = Project.first
+      user = User.first
+      todo = Todo.create(name: 'todo_test',description: "todo test sample",  project_id: project.id , user_id: user.id)
+      expect(todo.status).to eq('new')
+    end
+  end
+end
